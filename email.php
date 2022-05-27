b289b76bf14234ab8188a12d5f24fb975a373bd6
Erik Espinosa <erikespinosar@gmail.com>
2022-05-27 15:39:05 -0500

    fix: change email for contact form

--- email.php
+++ email.php
@@ -5,8 +5,8 @@
 
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-		$to = 'jimmy@jswyasociados.com';
-		$from = 'jimmy@jswyasociados.com';
+		$to = 'admin@jswyasociados.com';
+		$from = 'admin@jswyasociados.com';
 		$name = trim($_POST["name"]);
         $phone = trim($_POST["phone"]);
 		$email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
@@ -26,14 +26,14 @@
 						padding: 2rem; 
 						border: 1px solid #b5b5b5; 
 						box-sizing: border-box;">
-					<h1>Centro empresarial MDM</h1>
+					<h1>JSW y Asociados</h1>
 					<p><strong>Asunto:</strong> ' . $subject . '</p>
 					<p><strong>Nombre:</strong> ' . $name . '</p>
 					<p><strong>Teléfono:</strong> ' . $phone . '</p>
 					<p><strong>Correo electrónico:</strong> <a href="mailto:"' . $email . '">' . $email . '</a></p>
 					<p><strong>Mensaje:</strong> ' . $message . '</p>
 					<hr>
-					<small>Correo electríctrínico enviado desde el sitio web.</small>
+					<small>Correo electrónico enviado desde el sitio web.</small>
 					</div>';
 		
 		$headers = "MIME-Version: 1.0" . "\r\n";
